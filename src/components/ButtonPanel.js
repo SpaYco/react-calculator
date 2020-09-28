c5f0fa6a1f2c82be4da8fc612f220ae0f029ff01
SpaYco <zizou.aziz101@gmail.com>
2020-09-28 19:25:53 +0100

    added ButtonPanel stylings

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,74 +1,103 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const styles = {
+  display: 'flex',
+  flexDirection: 'column',
+};
+
+const rowStyles = {
+  height: 100,
+  width: '100%',
+  display: 'flex',
+};
+
+const btnStyles = {
+  width: '25%',
+  backgroundColor: '',
+};
+
+let btnWide;
 
 // eslint-disable-next-line  react/prefer-stateless-function
 class ButtonPanel extends React.Component {
   render() {
+    const { color } = this.props;
+    const { wide } = this.props;
+
+    if (wide === true) {
+      btnWide = '50%';
+    } else {
+      btnWide = '25%';
+    }
+
+    btnStyles.backgroundColor = color;
     return (
-      <div>
-        <div>
-          <button type="submit">
+      <div style={styles}>
+        <div style={rowStyles}>
+          <button type="submit" style={btnStyles}>
             AC
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             +/-
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             %
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             ÷
           </button>
         </div>
-        <div>
-          <button type="submit">
+        <div style={rowStyles}>
+          <button type="submit" style={btnStyles}>
             7
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             8
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             9
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             X
           </button>
         </div>
-        <div>
-          <button type="submit">
+        <div style={rowStyles}>
+          <button type="submit" style={btnStyles}>
             4
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             5
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             6
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             -
           </button>
         </div>
-        <div>
-          <button type="submit">
+        <div style={rowStyles}>
+          <button type="submit" style={btnStyles}>
             1
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             2
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             3
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             +
           </button>
         </div>
-        <div>
-          <button type="submit">
+        <div style={rowStyles}>
+          <button type="submit" style={{ width: btnWide, backgroundColor: color }}>
             0
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             .
           </button>
-          <button type="submit">
+          <button type="submit" style={btnStyles}>
             =
           </button>
         </div>
@@ -77,5 +106,15 @@ class ButtonPanel extends React.Component {
     );
   }
 }
+
+ButtonPanel.defaultProps = {
+  color: 'orange',
+  wide: true,
+};
+
+ButtonPanel.propTypes = {
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
 
 export default ButtonPanel;
