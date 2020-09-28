2c350f0e8325605b4676e21e98cedec6d9c42b85
SpaYco <zizou.aziz101@gmail.com>
2020-09-28 20:19:21 +0100

    fixed eslint

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,15 @@
-/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Button extends React.Component {
   render() {
+
+    const { name } = this.props;
+
     return (
       <div>
-        {this.props.name}
+        {name}
       </div>
     );
   }
