7f0634b2e41c38264e8201cbff7a349af3c529a4
SpaYco <zizou.aziz101@gmail.com>
2020-09-28 20:21:23 +0100

    fixed eslint

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 // eslint-disable-next-line react/prefer-stateless-function
 class Button extends React.Component {
   render() {
-
     const { name } = this.props;
 
     return (
