32bbb1818168b3f41a167d8966a709f8eb49e1c8
SpaYco <zizou.aziz101@gmail.com>
2020-09-29 15:04:55 +0100

    added operate method

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,7 +1,25 @@
-const calculate = (data, btn) => {
-  if (btn === '=') {
-    return { total: data.total };
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  const bigOne = Big(numberOne);
+  const bigTwo = Big(numberTwo);
+  let result;
+  if (operation === '+') {
+    result = bigOne.plus(bigTwo).toString();
   }
+  if (operation === '-') {
+    result = bigOne.minus(bigTwo).toString();
+  }
+  if (operation === 'X') {
+    result = bigOne.times(bigTwo).toString();
+  }
+  if (operation === '÷') {
+    result = bigOne.div(bigTwo).toString();
+  }
+  if (operation === '%') {
+    result = bigOne.div(bigTwo).toString();
+  }
+  return result;
 };
 
-export default calculate;
+export default operate;
